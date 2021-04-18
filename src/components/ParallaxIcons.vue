bdd3dae5d433bfb59069f9128b816512213755c7
Ankit <ankitanand.palli@quest-global.com>
2021-04-18 19:33:23 +0530

    icons

--- src/components/ParallaxIcons.vue
+++ src/components/ParallaxIcons.vue
@@ -2,13 +2,13 @@
   <div class="container col-6 d-md-block d-sm-none d-none">
     <!-- Top Icons -->
     <img src="../assets/angular.svg" data-speed="1" width="80px" class="layer" style="left:-250px;top:-100px;"/>
-    <img src="../assets/node.svg" data-speed="1" width="60px" class="layer" style="left:70%;top:-100px"/>
+    <img src="../assets/node.svg" data-speed="1" width="60px" class="layer" style="right:-250px;top:-100px"/>
     <!-- Center Icons -->
     <img src="../assets/react.svg" data-speed="2" width="120px" class="layer" style="left:20%;top:250px"/> 
     <img src="../assets/aws.png" data-speed="3" width="100px" class="layer" style="left:20%;top:50px"/> 
     <!-- Bottom Icons -->
-    <img src="../assets/android.png" data-speed="1" width="90px" class="layer" style="left:70%;top:400px"/> 
-    <img src="../assets/jenkins.png" data-speed="2" width="80px" class="layer" style="left:2%;top:400px"/> 
+    <img src="../assets/android.png" data-speed="1" width="90px" class="layer" style="right:-200px;top:400px"/> 
+    <img src="../assets/jenkins.png" data-speed="2" width="80px" class="layer" style="left:-200px;top:400px"/> 
 
   </div>
 </template>
